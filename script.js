561139034c29b8842d424749eaed0914bff6616d
Zohra AIT ABA <youbi.zohra@gmail.com>
2023-06-27 10:24:21 +0200

    add Footer

---
+++ script.js
@@ -0,0 +1,6 @@
+var buttonTiti= document.getElementById("buttontiti");
+var inputTiti= document.getElementById("inputTiti");
+
+buttonTiti.addEventListener("click", function (){
+    inputTiti.textContent= "titi"
+});
